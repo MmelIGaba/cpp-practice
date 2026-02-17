bf75adc7299551cbc58f659e11f8ef54c436783b
Mmeli <mmeligabrieldyantyi@gmail.com>
2026-02-17 09:25:22 +0200

    C++ and JS

---
+++ CircularArray.cpp
@@ -0,0 +1,21 @@
+#inlcude <iostream>
+#include <vector>
+
+/*
+ * Complete the 'circularArrayRotation' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY queries
+ */
+
+vector<int> circularArrayRotation(vector<int> a, int k, vector<int> queries) {
+
+}
+
+int main(){
+
+	return 0;
+}
